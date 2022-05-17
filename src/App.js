5a5955007ff44f965cb8a08690daf8b7681df2be
Adrian Bona <adrianbona@outlook.com>
2022-05-17 14:48:45 -0300

    Remove console warnings

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import DimmerLoader from "components/DimmerOverlay";
 import ErrorOverlay from "components/ErrorOverlay";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Container } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 import { aliceNetAdapter } from "./adapter/alicenetadapter";
-import { AliceNetMenu, Footer } from "./components";
-import { Home, BlockExplorer, DataExplorer, TxExplorer } from "./pages";
+import { AliceNetMenu, Footer } from "./components";
+import { BlockExplorer, DataExplorer, Home, TxExplorer } from "./pages";
 import { aliceNetWalletEqualize } from "./redux/reducers";
 
 function App() {
@@ -17,7 +17,6 @@ function App() {
             <Router>
                 <AliceNetMenu />
                 <Switch>
-                    {/* <Route path="/" component={Test} /> */}
                     <Route exact path={["/blocks", "/"]} component={Home} />
                     <Route exact path="/test" component={Test} />
                     <Route exact path="/block" component={BlockExplorer} />
@@ -38,25 +37,23 @@ function Test() {
     let [block, setBlock] = React.useState({})
 
     //let walletState = useSelector(state => state.aliceNetWallet);
-    // Must be used to propogate UI updates from the class being watched  
+    // Must be used to propagate UI updates from the class being watched
     // -- State is minified as a string and serialized to represent state changes but will not be accessible as an object tree 
-    // Wallet accounts won't work without connecting the serialized state to propgate UI updates
+    // Wallet accounts won't work without connecting the serialized state to propagate UI updates
 
-    // console.log("MINIFIED/SERIALIZED STATE", walletState) // Check out the console to see the minified state
-
-    const adapterState = useSelector(state => state.aliceNetAdapter); // If normal serializeable state is to be read directly we can assign to a var 
-    const dispatch = useDispatch()
+    const adapterState = useSelector(state => state.aliceNetAdapter); // If normal serializable state is to be read directly we can assign to a var
+    const dispatch = useDispatch();
 
     const getBlock = async (blockNum) => {
         let block = await aliceNetAdapter.getBlock(blockNum);
         setBlock(block);
-        console.log(block)
+        console.log(block);
     }
 
     const getCurrentBlock = async () => {
         let block = await aliceNetAdapter.getCurrentBlock();
         setBlock(block);
-        console.log(block)
+        console.log(block);
     }
 
     const addRandomAccount = async () => {
@@ -71,7 +68,7 @@ function Test() {
     }
 
     const printDataStoresForAddress = async (address, curve = 1) => {
-        let dstores = await aliceNetAdapter.getDataStoresForAddres(address, curve)
+        let dstores = await aliceNetAdapter.getDataStoresForAddres(address, curve);
         console.log(dstores);
     }
 
@@ -82,7 +79,8 @@ function Test() {
             {aliceNetAdapter.wallet.Account.accounts.length}
 
             <br />
-            <button onClick={() => console.log(aliceNetAdapter)}>Print Adapter Instance</button><br />
+            <button onClick={() => console.log(aliceNetAdapter)}>Print Adapter Instance</button>
+            <br />
             <button onClick={addRandomAccount}>addRandomAccount</button>
 
             <h2>Connection State</h2>
@@ -99,19 +97,22 @@ function Test() {
             <button onClick={() => aliceNetAdapter.startMonitoringBlocks()}>Start Monitor</button>
             <button onClick={() => aliceNetAdapter.stopMonitoringBlocks()}>Stop Monitor</button>
             <button onClick={() => aliceNetAdapter.resetBlockMonitor()}>Reset Monitor</button>
-            {aliceNetAdapter.blocks.map(block => {
 
-                return (<div key={block["BClaims"].Height}>{block["BClaims"].Height}</div>)
-            })}
+            {aliceNetAdapter.blocks.map(block =>
+                <div key={block["BClaims"].Height}>{block["BClaims"].Height}</div>
+            )}
 
             <h4>Get Current Block</h4>
 
-            <button onClick={() => getCurrentBlock()}>Get Block: Current: {block?.BClaims?.Height}</button> <br />
+            <button onClick={() => getCurrentBlock()}>Get Block: Current: {block?.BClaims?.Height}</button>
+            <br />
             <button onClick={() => getBlock(178000)}>Get Block: 178000: {block?.BClaims?.Height}</button>
 
             <h4>DataStores</h4>
-            <button onClick={() => printDataStoresForAddress("eeacfc737e72fdf2518fb58c0a620f783eb2515f")}>Get Datastores for address (See code)</button> <br />
-
+            <button onClick={() => printDataStoresForAddress("eeacfc737e72fdf2518fb58c0a620f783eb2515f")}>
+                Get Datastores for address (See code)
+            </button>
+            <br />
 
         </div>
     )
--- src/components/search/AliceNetSearch.jsx
+++ src/components/search/AliceNetSearch.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react';
-import { Dropdown, Button } from 'semantic-ui-react';
+import { useState } from "react";
+import { Button, Dropdown } from "semantic-ui-react";
 import { useHistory } from "react-router-dom";
-import { ReactComponent as Icon1 } from 'assets/searchIcon1.svg';
-import { ReactComponent as Icon2 } from 'assets/searchIcon2.svg';
-import { ReactComponent as Icon3 } from 'assets/searchIcon3.svg';
-import styles from './AliceNetSearch.module.scss';
+import { ReactComponent as Icon1 } from "assets/searchIcon1.svg";
+import { ReactComponent as Icon2 } from "assets/searchIcon2.svg";
+import { ReactComponent as Icon3 } from "assets/searchIcon3.svg";
+import styles from "./AliceNetSearch.module.scss";
 
 const options = [
-    { key: 1, text: 'Block', value: 1 },
-    { key: 2, text: 'Tx', value: 2 },
-  ]
+    { text: 'Block', value: 1 },
+    { text: 'Tx', value: 2 },
+];
 
-export function AliceNetSearch(){
+export function AliceNetSearch() {
     const history = useHistory();
-    const [term, setTerm] = useState('')
-    const [option, setOption] = useState(options[0].value)
+    const [term, setTerm] = useState('');
+    const [selectedOption, setSelectedOption] = useState(options[0]);
 
-    const handleChange = (e, { value }) => setOption(value)
+    const handleChange = (e, { value }) => {
+        setSelectedOption(options[value - 1]);
+    };
 
     const handleSearch = () => {
-        switch(option){
+        switch (selectedOption.value) {
             case options[0].value:
                 history.push(`/block?height=${term}`);
                 break;
@@ -27,9 +29,9 @@ export function AliceNetSearch(){
                 history.push(`/tx?hash=${term}`);
                 break;
             default:
-                alert('Invalid option')
+                alert('Invalid option');
         }
-    }
+    };
 
     return <div className={styles.container}>
         <div className={styles.content}>
@@ -38,28 +40,37 @@ export function AliceNetSearch(){
                     <h2 className={styles.headers}>Explore the Madnet blockchain</h2>
                 </div>
                 <div>
-                    <Dropdown 
-                        onChange={handleChange}
+                    <Dropdown
+                        text={selectedOption.text}
                         className={styles.dropdown}
-                        options={options}
-                        value={option}
                     >
                         <Dropdown.Menu className={styles.menu}>
-                            <Dropdown.Item onClick={handleChange} value={options[0].value} className={styles.option}>{options[0].text}</Dropdown.Item>
-                            <Dropdown.Item onClick={handleChange} value={options[1].value} className={styles.option}>{options[1].text}</Dropdown.Item>
+                            {options.map(option => {
+                                    return (<Dropdown.Item
+                                        key={`header-option-${option.value}`}
+                                        onClick={handleChange}
+                                        value={option.value}
+                                        className={styles.option}
+                                    >
+                                        {option.text}
+                                    </Dropdown.Item>);
+                                }
+                            )}
                         </Dropdown.Menu>
                     </Dropdown>
-                    <input placeholder={option === options[0].value ? 'Block number' : 'Tx hash'} 
-                            className={styles.input}
-                            value={term} 
-                            onChange={(e) => setTerm(e.target.value)}/>
+                    <input
+                        placeholder={selectedOption.value === options[0].value ? 'Block number' : 'Tx hash'}
+                        className={styles.input}
+                        value={term}
+                        onChange={(e) => setTerm(e.target.value)}
+                    />
                     <Button className={styles.button} onClick={() => handleSearch(term)}>Search</Button>
                 </div>
             </div>
             <div className={styles.iconContainer}>
-                <Icon1 className={styles.icon}/>
-                <Icon2 className={styles.icon}/>
-                <Icon3 className={styles.icon}/>
+                <Icon1 className={styles.icon} />
+                <Icon2 className={styles.icon} />
+                <Icon3 className={styles.icon} />
             </div>
         </div>
     </div>
