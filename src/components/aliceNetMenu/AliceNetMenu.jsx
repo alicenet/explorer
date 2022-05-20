9333796f41fe42cebc12ebcb5573af20492f71c8
Adrian Bona <adrianbona@outlook.com>
2022-05-20 13:17:00 -0300

    EsLint

--- src/components/aliceNetMenu/AliceNetMenu.jsx
+++ src/components/aliceNetMenu/AliceNetMenu.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Link, useLocation, useHistory } from "react-router-dom";
-import { Menu, Image, Dropdown } from 'semantic-ui-react';
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { Dropdown, Image, Menu } from 'semantic-ui-react';
 import Logo from "assets/MadNetwork Logo Horizontal GRAYSCALE.png";
 import styles from './AliceNetMenu.module.scss';
 
 //TODO define where to get this
 const GITHUB_URL = 'https://github.com/madhive';
-const WHITEPAPER_URL = 'https://www.madnetwork.com/madnetwork-download-whitepaper';
+const WHITE_PAPER_URL = 'https://www.madnetwork.com/madnetwork-download-whitepaper';
 const COMMUNITY_URL = 'https://www.madnetwork.com/';
-//TODO define where to get this
-const WALLETS_PLACEHOLDER =[ 'wallet0', 'wallet1'] 
 
-export function AliceNetMenu(props) {
+//TODO define where to get this
+const WALLETS_PLACEHOLDER = ['wallet0', 'wallet1'];
+
+export function AliceNetMenu() {
 
     const location = useLocation();
     const history = useHistory();
 
     return (
         <Menu pointing secondary className={styles.menu}>
-            <Menu.Menu position='left'>
-                <Image src={Logo} width='205' className={styles.image} as={Link} to="" onClick={() => history.push('/')} />
+            <Menu.Menu position="left">
+                <Image
+                    src={Logo}
+                    width="205"
+                    className={styles.image}
+                    as={Link}
+                    onClick={() => history.push('/')}
+                />
             </Menu.Menu>
-            <Menu.Menu position='right'>
+            <Menu.Menu position="right">
                 <Menu.Item
                     as={Link}
                     to="block"
@@ -34,9 +41,9 @@ export function AliceNetMenu(props) {
                     name="About"
                     active={location.pathname.slice(1) === 'about'}
                 />
-                <Dropdown item text='Wallet Download'>
+                <Dropdown item text="Wallet Download">
                     <Dropdown.Menu>
-                        {WALLETS_PLACEHOLDER.map(wallet => 
+                        {WALLETS_PLACEHOLDER.map(wallet =>
                             <Dropdown.Item key={wallet}>{wallet}</Dropdown.Item>
                         )}
                     </Dropdown.Menu>
@@ -46,8 +53,8 @@ export function AliceNetMenu(props) {
                     name="Github"
                 />
                 <Menu.Item
-                    onClick={() => window.open(WHITEPAPER_URL, '_blank').focus()}
-                    name="Whitepaper"
+                    onClick={() => window.open(WHITE_PAPER_URL, '_blank').focus()}
+                    name="White Paper"
                 />
                 <Menu.Item
                     onClick={() => window.open(COMMUNITY_URL, '_blank').focus()}
@@ -55,5 +62,5 @@ export function AliceNetMenu(props) {
                 />
             </Menu.Menu>
         </Menu>
-    )
+    );
 }
