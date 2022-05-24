c575f2335355def1d9c7e5fc937ad893d5b6c4e7
Adrian Bona <adrianbona@outlook.com>
2022-05-24 16:02:56 -0300

    Footer was updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,5 @@
-import { ReactComponent as DiscordIcon } from 'assets/discord-icon.svg';
-import { ReactComponent as TwitterIcon } from 'assets/twitter-icon.svg';
-import styles from './Footer.module.scss';
+import { ReactComponent as DiscordIcon } from "assets/discord-icon.svg";
+import { ReactComponent as TwitterIcon } from "assets/twitter-icon.svg";
 import { Container } from "semantic-ui-react";
 import React from "react";
 
@@ -13,19 +12,44 @@ export function Footer() {
         <Container fluid className="flex justify-between py-10">
 
             <div className="flex gap-5">
+
                 <span>Follow us on:</span>
-                <TwitterIcon width={15} className={styles.svg} />
-                <DiscordIcon width={15} className={styles.svg} />
+                <TwitterIcon className={`w-5 cursor-pointer hover:opacity-50 fill-current text-white`} />
+                <DiscordIcon className={`w-5 cursor-pointer hover:opacity-50 fill-current text-white`} />
+
             </div>
 
             <div className="flex gap-3">
-                <span onClick={() => window.location = "https://madnetwork.com"}>About</span>
-                <MenuDivider/>
-                <span onClick={() => window.location = "https://madnetwork.com"}>Legal</span>
-                <MenuDivider/>
-                <span onClick={() => window.location = "https://madnetwork.com"}>Terms of service</span>
-                <MenuDivider/>
+
+                <span
+                    className="cursor-pointer hover:opacity-50"
+                    onClick={() => window.location = "https://madnetwork.com"}
+                >
+                    About
+                </span>
+
+                <MenuDivider />
+
+                <span
+                    className="cursor-pointer hover:opacity-50"
+                    onClick={() => window.location = "https://madnetwork.com"}
+                >
+                    Legal
+                </span>
+
+                <MenuDivider />
+
+                <span
+                    className="cursor-pointer hover:opacity-50"
+                    onClick={() => window.location = "https://madnetwork.com"}
+                >
+                    Terms of service
+                </span>
+
+                <MenuDivider />
+
                 <span className="text-lightgray">AliceNet Inc © {new Date().getFullYear()}</span>
+
             </div>
 
         </Container>
