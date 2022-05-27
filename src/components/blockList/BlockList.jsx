0195e6441fa709db81271113d8ce575c11673d67
Adrian Bona <adrianbona@outlook.com>
2022-05-27 16:50:14 -0300

    Not needed

--- src/components/blockList/BlockList.jsx
+++ src/components/blockList/BlockList.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import { Button, Grid, Icon } from "semantic-ui-react"
 import { copyText } from "utils";
 import { content, HelpTooltip } from "components";
-import styles from './BlockList.module.scss';
+import styles from "./BlockList.module.scss";
 
-export function BlockList({ height, txCount, prevBlock, txRoot, stateRoot, headerRoot, sigGroup, handleBlockNavLeft, handleBlockNavRight, maxHeight = Number.MAX_VALUE}) {
+export function BlockList({
+    height,
+    txCount,
+    prevBlock,
+    txRoot,
+    stateRoot,
+    headerRoot,
+    sigGroup,
+    handleBlockNavLeft,
+    handleBlockNavRight,
+    maxHeight = Number.MAX_VALUE
+}) {
 
     return (
+
         <Grid className={styles.blockList} padded="vertically">
             <Grid.Row className={styles.row}>
                 <Grid.Column className={styles.column} width={3}>
@@ -16,31 +28,31 @@ export function BlockList({ height, txCount, prevBlock, txRoot, stateRoot, heade
                 <Grid.Column className={styles.column} width={12}>
                     {height}
                     <>
-                        {height > 1 && 
-                            <Button
-                                className={styles.button} 
-                                icon 
-                                onClick={handleBlockNavLeft}
-                            >
-                                <Icon 
-                                    className={styles.navIcon} 
-                                    name="chevron left" 
-                                    size="small" 
-                                />
-                            </Button>
+                        {height > 1 &&
+                        <Button
+                            className={styles.button}
+                            icon
+                            onClick={handleBlockNavLeft}
+                        >
+                            <Icon
+                                className={styles.navIcon}
+                                name="chevron left"
+                                size="small"
+                            />
+                        </Button>
                         }
-                        {(maxHeight > height) && 
-                            <Button 
-                                className={styles.button}
-                                icon 
-                                onClick={handleBlockNavRight}
-                            >
-                                <Icon 
-                                    className={styles.navIcon} 
-                                    name="chevron right" 
-                                    size="small" 
-                                />
-                            </Button>
+                        {(maxHeight > height) &&
+                        <Button
+                            className={styles.button}
+                            icon
+                            onClick={handleBlockNavRight}
+                        >
+                            <Icon
+                                className={styles.navIcon}
+                                name="chevron right"
+                                size="small"
+                            />
+                        </Button>
                         }
                     </>
                 </Grid.Column>
@@ -50,7 +62,7 @@ export function BlockList({ height, txCount, prevBlock, txRoot, stateRoot, heade
                 <Grid.Column className={styles.column} width={3}>
                     <HelpTooltip content={content.txCount} />
                     <p>Transaction Count</p>
-                </Grid.Column> 
+                </Grid.Column>
                 <Grid.Column className={styles.column} width={12}>
                     <p>{txCount ? txCount : 0}</p>
                 </Grid.Column>
@@ -63,10 +75,10 @@ export function BlockList({ height, txCount, prevBlock, txRoot, stateRoot, heade
                 </Grid.Column>
                 <Grid.Column className={styles.column} width={12}>
                     <p>{`0x${prevBlock}`}</p>
-                    <Icon 
-                        name="copy outline" 
-                        className="click" 
-                        onClick={() => copyText("0x" + prevBlock)} 
+                    <Icon
+                        name="copy outline"
+                        className="click"
+                        onClick={() => copyText(prevBlock)}
                     />
                 </Grid.Column>
             </Grid.Row>
@@ -78,10 +90,10 @@ export function BlockList({ height, txCount, prevBlock, txRoot, stateRoot, heade
                 </Grid.Column>
                 <Grid.Column className={styles.column} width={12}>
                     <p>{`0x${txRoot}`}</p>
-                    <Icon 
-                        name="copy outline" 
-                        className="click" 
-                        onClick={() => copyText("0x" + txRoot)} 
+                    <Icon
+                        name="copy outline"
+                        className="click"
+                        onClick={() => copyText(txRoot)}
                     />
                 </Grid.Column>
             </Grid.Row>
@@ -89,14 +101,14 @@ export function BlockList({ height, txCount, prevBlock, txRoot, stateRoot, heade
             <Grid.Row className={styles.row}>
                 <Grid.Column className={styles.column} width={3}>
                     <HelpTooltip content={content.stateRoot} />
-                        <p>State Root</p>
-                    </Grid.Column>
+                    <p>State Root</p>
+                </Grid.Column>
                 <Grid.Column className={styles.column} width={12}>
                     <p>{`0x${stateRoot}`}</p>
-                    <Icon 
-                        name="copy outline" 
-                        className="click" 
-                        onClick={() => copyText("0x" + stateRoot)} 
+                    <Icon
+                        name="copy outline"
+                        className="click"
+                        onClick={() => copyText(stateRoot)}
                     />
                 </Grid.Column>
             </Grid.Row>
@@ -108,10 +120,10 @@ export function BlockList({ height, txCount, prevBlock, txRoot, stateRoot, heade
                 </Grid.Column>
                 <Grid.Column className={styles.column} width={12}>
                     <p>{`0x${headerRoot}`}</p>
-                    <Icon 
-                        name="copy outline" 
-                        className="click" 
-                        onClick={() => copyText("0x" + headerRoot)} 
+                    <Icon
+                        name="copy outline"
+                        className="click"
+                        onClick={() => copyText(headerRoot)}
                     />
                 </Grid.Column>
             </Grid.Row>
@@ -123,14 +135,15 @@ export function BlockList({ height, txCount, prevBlock, txRoot, stateRoot, heade
                 </Grid.Column>
                 <Grid.Column className={styles.column} width={12}>
                     <p>{`0x${sigGroup}`}</p>
-                    <Icon 
-                        name="copy outline" 
-                        className="click" 
-                        onClick={() => copyText("0x" + sigGroup)} 
+                    <Icon
+                        name="copy outline"
+                        className="click"
+                        onClick={() => copyText(sigGroup)}
                     />
                 </Grid.Column>
             </Grid.Row>
         </Grid>
+
     );
 
 }
