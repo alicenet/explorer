c04e9f28f03c40d163aff646806df41132422463
Adrian Bona <adrianbona@outlook.com>
2022-06-05 11:09:36 -0300

    Update Netlify (#40)
    * Check if height is valid
    
    * Move to utils
    
    * Handle navigation arrows on block explorer
    
    * add show more datastore togle
    
    * Fix boolean parsing
    
    * Add border control to arrows
    
    * Add valid check
    
    * refactor: datastore button text
    
    * Specializing row content rendering (#32)
    
    * Handling tables content
    
    * Community link is no longer needed
    
    * Not needed
    
    * Missing index
    
    * Moving from Pages to Components directory
    
    * Not needed
    
    * PR changes
    
    * Remove arrow
    
    * Better wording
    
    * Linking block numbers to sections
    
    * Not needed
    
    * Spacing
    
    * Duplicated line
    
    * Updated URL format
    
    * Removing query-string package for good
    
    * Refactor files
    
    * Hide the overlay for warnings in a React
    
    * Reducing re renders
    
    * Running Yarn
    
    * BlockList refactored
    
    Co-authored-by: RicardoFrabasile <rfrabasile@gmail.com>
    Co-authored-by: Everton Renan <everton.renan@clevertech.biz>
    Co-authored-by: ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>

---
+++ src/components/blockList/BlockList.jsx
@@ -0,0 +1,182 @@
+import React from "react";
+import { Button, Grid, Icon } from "semantic-ui-react"
+import { content, HelpTooltip } from "components";
+import { useHistory } from "react-router-dom";
+import { aliceNetAdapter } from "adapter/alicenetadapter";
+import { copyText } from "utils";
+
+export function BlockList({ blockInfo }) {
+
+    const { BClaims, SigGroup: sigGroup } = blockInfo;
+    const {
+        Height: height,
+        TxCount: txCount,
+        PrevBlock: prevBlock,
+        TxRoot: txRoot,
+        StateRoot: stateRoot,
+        HeaderRoot: headerRoot
+    } = BClaims;
+
+    const history = useHistory();
+    const handleBlockNav = (term) => history.push(`/block/${term}`);
+
+    const maxHeight = aliceNetAdapter.blocks[0]?.BClaims.Height;
+
+    return (
+
+        <Grid padded="vertically" columns={"equal"}>
+
+            <Grid.Row className="px-3 bg-rowblack" columns={2}>
+
+                <Grid.Column className="flex items-center gap-5" width={3}>
+                    <HelpTooltip content={content.height} />
+                    <p>Block Height</p>
+                </Grid.Column>
+
+                <Grid.Column className="flex items-center gap-5">
+                    {height}
+                    <>
+                        {height > 1 &&
+                        <Button
+                            icon
+                            className="flex items-center bg-buttonblack text-white px-1 py-2 m-0 hover:opacity-80"
+                            onClick={() => handleBlockNav(height - 1)}
+                        >
+                            <Icon
+                                className="m-0 p-0"
+                                name="chevron left"
+                                size="small"
+                            />
+                        </Button>
+                        }
+                        {(maxHeight > height) &&
+                        <Button
+                            icon
+                            className="flex items-center bg-buttonblack text-white px-1 py-2 m-0 hover:opacity-80"
+                            onClick={() => handleBlockNav(height + 1)}
+                        >
+                            <Icon
+                                className="m-0 p-0"
+                                name="chevron right"
+                                size="small"
+                            />
+                        </Button>
+                        }
+                    </>
+                </Grid.Column>
+
+            </Grid.Row>
+
+            <Grid.Row className="px-3 bg-rowblack" columns={2}>
+
+                <Grid.Column className="flex items-center gap-5" width={3}>
+                    <HelpTooltip content={content.txCount} />
+                    <p>Transaction Count</p>
+                </Grid.Column>
+
+                <Grid.Column className="flex items-start gap-5">
+                    <p>{txCount ? txCount : 0}</p>
+                </Grid.Column>
+
+            </Grid.Row>
+
+            <Grid.Row className="px-3 bg-rowblack" columns={2}>
+
+                <Grid.Column className="flex items-center gap-5" width={3}>
+                    <HelpTooltip content={content.previousBlock} />
+                    <p>Previous Block</p>
+                </Grid.Column>
+
+                <Grid.Column>
+                    <div
+                        className="flex items-start gap-5 cursor-pointer hover:opacity-80"
+                        onClick={() => copyText(prevBlock)}
+                    >
+                        <p>{`0x${prevBlock}`}</p>
+                        <Icon name="copy outline" />
+                    </div>
+                </Grid.Column>
+
+            </Grid.Row>
+
+            <Grid.Row className="px-3 bg-rowblack" columns={2}>
+
+                <Grid.Column className="flex items-center gap-5" width={3}>
+                    <HelpTooltip content={content.txRoot} />
+                    <p>Transaction Root</p>
+                </Grid.Column>
+
+                <Grid.Column>
+                    <div
+                        className="flex items-start gap-5 cursor-pointer hover:opacity-80"
+                        onClick={() => copyText(txRoot)}
+                    >
+                        <p>{`0x${txRoot}`}</p>
+                        <Icon name="copy outline" />
+                    </div>
+                </Grid.Column>
+
+            </Grid.Row>
+
+            <Grid.Row className="px-3 bg-rowblack" columns={2}>
+
+                <Grid.Column className="flex items-center gap-5" width={3}>
+                    <HelpTooltip content={content.stateRoot} />
+                    <p>State Root</p>
+                </Grid.Column>
+
+                <Grid.Column>
+                    <div
+                        className="flex items-start gap-5 cursor-pointer hover:opacity-80"
+                        onClick={() => copyText(stateRoot)}
+                    >
+                        <p>{`0x${stateRoot}`}</p>
+                        <Icon name="copy outline" />
+                    </div>
+                </Grid.Column>
+
+            </Grid.Row>
+
+            <Grid.Row className="px-3 bg-rowblack" columns={2}>
+
+                <Grid.Column className="flex items-center gap-5" width={3}>
+                    <HelpTooltip content={content.headerRoot} />
+                    <p>Header Root</p>
+                </Grid.Column>
+
+                <Grid.Column>
+                    <div
+                        className="flex items-start gap-5 cursor-pointer hover:opacity-80"
+                        onClick={() => copyText(headerRoot)}
+                    >
+                        <p>{`0x${headerRoot}`}</p>
+                        <Icon name="copy outline" />
+                    </div>
+                </Grid.Column>
+
+            </Grid.Row>
+
+            <Grid.Row className="px-3 bg-rowblack" columns={2}>
+
+                <Grid.Column className="flex items-center gap-5" width={3}>
+                    <HelpTooltip content={content.groupSignature} />
+                    <p>Group Signature</p>
+                </Grid.Column>
+
+                <Grid.Column>
+                    <div
+                        className="flex items-start gap-5 cursor-pointer hover:opacity-80 break-all"
+                        onClick={() => copyText(sigGroup)}
+                    >
+                        <p>{`0x${sigGroup}`}</p>
+                        <Icon name="copy outline" />
+                    </div>
+                </Grid.Column>
+
+            </Grid.Row>
+
+        </Grid>
+
+    );
+
+}
