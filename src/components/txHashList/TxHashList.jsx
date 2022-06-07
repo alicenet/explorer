7371c362188a968ab876976048c6bf9df6a79317
Adrian Bona <adrianbona@outlook.com>
2022-06-07 12:04:42 -0300

    Removing unsued styling

--- src/components/txHashList/TxHashList.jsx
+++ src/components/txHashList/TxHashList.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Icon, Grid } from "semantic-ui-react";
+import { Grid, Icon } from "semantic-ui-react";
 import { copyText } from "utils";
-import styles from './TxHashList.module.scss';
 
-export function TxHashList({ txHshLst, txViewLink = '/' }) {
-    if (!txHshLst) {
-        return (<p></p>);
-    }
+export function TxHashList({ txHshLst }) {
 
     return (
-        <Grid className={styles.txHashList} padded="vertically">
-            {txHshLst && txHshLst.map((hash, index) => {
-                return (
-                    <Grid.Row key={index} className={styles.row}>
-                        <Grid.Column width={2}>Tx Hash</Grid.Column>
-                        <Grid.Column width={13}>
-                            {/* TODO add link url  */}
-                            <Link to={txViewLink}>0x{hash}</Link>
-                            <Icon 
-                                name="copy outline" 
-                                className="click" 
-                                onClick={() => copyText("0x" + hash)} 
-                            />
-                        </Grid.Column>
-                    </Grid.Row>
-                )
-            })}
+
+        <Grid padded="vertically" className="mx-0 break-words" columns={"equal"}>
+
+            {txHshLst && txHshLst.map((hash, index) =>
+
+                <Grid.Row key={`row-hash-${index}`} className="px-6 bg-rowblack" columns={2}>
+
+                    <Grid.Column className="flex items-center gap-5 p-0" width={3}>
+                        Tx Hash
+                    </Grid.Column>
+
+                    <Grid.Column className="p-0">
+                        <div className="flex items-start gap-5 cursor-pointer hover:opacity-80">
+                            <Link className="text-neongreen hover:text-neongreen hover:opacity-80" to={`/tx/${hash}`}>
+                                {`0x${hash}`}
+                            </Link>
+                            <Icon name="copy outline" onClick={() => copyText(hash)} />
+                        </div>
+                    </Grid.Column>
+
+                </Grid.Row>
+
+            )}
+
         </Grid>
-    )
+
+    );
+
 }
