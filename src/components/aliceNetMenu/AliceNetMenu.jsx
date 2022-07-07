90c6c060d9d3c805513d44f6d775ba482270f4ca
Adrian Bona <adrianbona@outlook.com>
2022-07-07 17:47:21 -0300

    Menu update

--- src/components/aliceNetMenu/AliceNetMenu.jsx
+++ src/components/aliceNetMenu/AliceNetMenu.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Container, Image, Menu } from "semantic-ui-react";
-import Logo from "assets/MadNetwork Logo Horizontal GRAYSCALE.png";
-
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const WHITE_PAPER_URL = process.env.REACT_APP_WHITE_PAPER_URL;
-
-const MenuDivider = () => <div className="border-r border-gray-700 my-3" />;
+import { Container, Grid } from "semantic-ui-react";
+import { AliceNetMenuMobile } from "./AliceNetMenuMobile";
+import { AliceNetMenuDesktop } from "./AliceNetMenuDesktop";
 
 export function AliceNetMenu() {
 
@@ -14,54 +9,29 @@ export function AliceNetMenu() {
 
         <Container fluid className="sticky top-0 bg-dark opacity-90">
 
-            <Menu pointing secondary className="py-5">
+            <Grid>
 
-                <Menu.Menu position="left" className="self-center">
-                    <Image
-                        src={Logo}
-                        width="205px"
-                        style={{ filter: "invert(100%)" }}
-                        as={Link}
-                        to="/"
-                    />
-                </Menu.Menu>
+                <Grid.Row only="mobile">
 
-                <Menu.Menu position="right">
-                    <Menu.Item
-                        className="text-white hover:text-neongreen"
-                        as={Link}
-                        to="/"
-                        name="Monitor"
-                    />
-                    <MenuDivider />
-                    <Menu.Item
-                        className="text-white hover:text-neongreen"
-                        as={Link}
-                        to="/about"
-                        name="About"
-                    />
-                    <MenuDivider />
-                    <Menu.Item
-                        className="text-white hover:text-neongreen"
-                        as={Link}
-                        to="/"
-                        name="Wallet Download"
-                    />
-                    <MenuDivider />
-                    <Menu.Item
-                        className="text-white hover:text-neongreen"
-                        onClick={() => window.open(GITHUB_URL, '_blank').focus()}
-                        name="Github"
-                    />
-                    <MenuDivider />
-                    <Menu.Item
-                        className="text-white hover:text-neongreen"
-                        onClick={() => window.open(WHITE_PAPER_URL, '_blank').focus()}
-                        name="White Paper"
-                    />
-                </Menu.Menu>
+                    <Grid.Column>
 
-            </Menu>
+                        <AliceNetMenuMobile />
+
+                    </Grid.Column>
+
+                </Grid.Row>
+
+                <Grid.Row only="computer tablet">
+
+                    <Grid.Column>
+
+                        <AliceNetMenuDesktop />
+
+                    </Grid.Column>
+
+                </Grid.Row>
+
+            </Grid>
 
         </Container>
 
