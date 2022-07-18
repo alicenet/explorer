d046317b592ee88a749dd639ad0d99de0c21d014
Adrian Bona <adrianbona@outlook.com>
2022-07-18 11:03:24 -0300

    Menu updated

--- src/components/header/HeaderMobile.jsx
+++ src/components/header/HeaderMobile.jsx
@@ -42,8 +42,9 @@ export function HeaderMobile() {
             <Menu.Menu position="right">
 
                 <Modal
-                    basic
-                    dimmer="blurring"
+                    closeIcon
+                    size="fullscreen bg-dark"
+                    className="h-19/20"
                     onClose={() => setOpen(false)}
                     onOpen={() => setOpen(true)}
                     open={open}
@@ -55,9 +56,9 @@ export function HeaderMobile() {
                         />
                     }
                 >
-                    <div className="px-10">
+                    <div className="px-10 my-5">
 
-                        <Menu text vertical>
+                        <Menu text vertical className="flex items gap-5">
 
                             <Menu.Item
                                 style={{ textDecoration: "underline #00FFD1", textUnderlineOffset: "0.3em" }}
@@ -81,40 +82,43 @@ export function HeaderMobile() {
                                 onClick={() => setWalletMenuOpen(prevState => !prevState)}
                             />
 
-                            <Menu.Item className={
-                                classNames(
-                                    "px-5 transition-opacity duration-100 delay-100 py-0 my-0",
-                                    { "opacity-100": showWalletOptions },
-                                    { "opacity-0": !showWalletOptions },
-                                )
-                            }>
+                            {
+                                walletMenuOpen &&
+                                <Menu.Item className={
+                                    classNames(
+                                        "px-5 transition-opacity duration-100 delay-100 py-0 my-0",
+                                        { "opacity-100": showWalletOptions },
+                                        { "opacity-0": !showWalletOptions },
+                                    )
+                                }>
 
-                                {
-                                    showWalletOptions &&
-                                    <Menu text vertical>
+                                    {
+                                        showWalletOptions &&
+                                        <Menu text vertical>
 
-                                        <Menu.Item
-                                            className="text-white text-xl"
-                                            onClick={() => window.open(WALLET_LINUX_URL, '_blank').focus()}
-                                            name="Linux"
-                                        />
+                                            <Menu.Item
+                                                className="text-white text-xl"
+                                                onClick={() => window.open(WALLET_LINUX_URL, '_blank').focus()}
+                                                name="Linux"
+                                            />
 
-                                        <Menu.Item
-                                            className="text-white text-xl"
-                                            onClick={() => window.open(WALLET_MAC_URL, '_blank').focus()}
-                                            content="iOS"
-                                        />
+                                            <Menu.Item
+                                                className="text-white text-xl"
+                                                onClick={() => window.open(WALLET_MAC_URL, '_blank').focus()}
+                                                content="iOS"
+                                            />
 
-                                        <Menu.Item
-                                            className="text-white text-xl"
-                                            onClick={() => window.open(WALLET_WINDOWS_URL, '_blank').focus()}
-                                            name="Windows"
-                                        />
+                                            <Menu.Item
+                                                className="text-white text-xl"
+                                                onClick={() => window.open(WALLET_WINDOWS_URL, '_blank').focus()}
+                                                name="Windows"
+                                            />
 
-                                    </Menu>
-                                }
+                                        </Menu>
+                                    }
 
-                            </Menu.Item>
+                                </Menu.Item>
+                            }
 
                             <Menu.Item
                                 className="text-white text-2xl"
