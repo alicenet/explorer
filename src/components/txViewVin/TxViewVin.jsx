516faafa957103a1f463100a20ceee32cba9ec7a
Adrian Bona <adrianbona@outlook.com>
2022-07-22 08:53:04 -0300

    Vins updated

--- src/components/txViewVin/TxViewVin.jsx
+++ src/components/txViewVin/TxViewVin.jsx
@@ -13,9 +13,9 @@ export function TxViewVin({ txInfo }) {
 
                 <CollapsableCard title={`Vin ${index + 1}`} open={true} borderless key={`collapsable-tx-vin-${index}`}>
 
-                    <Grid padded="vertically" className="mx-0 break-words" columns={"equal"}>
+                    <Grid padded="vertically" className="mx-0 break-words" columns={"equal"} stackable>
 
-                        <Grid.Row className="px-6 bg-rowblack border-0 border-t border-tableblack" columns={2}>
+                        <Grid.Row className="px-6 bg-rowblack border-0 border-t border-tableblack mobile:p-2" columns={2}>
 
                             <Grid.Column className="flex items-center gap-3 p-0" width={4}>
                                 <HelpTooltip content={content.consumedTx} />
@@ -24,12 +24,12 @@ export function TxViewVin({ txInfo }) {
 
                             <Grid.Column className="p-0">
                                 <div className="flex items-start gap-3">
-                                    <p>{`0x${tx['TXInLinker']['TXInPreImage']['ConsumedTxHash']}`}</p>
+                                    <p className="break-all">{`0x${tx['TXInLinker']['TXInPreImage']['ConsumedTxHash']}`}</p>
                                     <Popup
                                         trigger={
                                             <Icon
                                                 name="copy outline"
-                                                className="cursor-pointer hover:opacity-80"
+                                                className="cursor-pointer hover:opacity-80 mobile:hidden"
                                                 onClick={() => copyText(tx['TXInLinker']['TXInPreImage']['ConsumedTxHash'])}
                                             />
                                         }
@@ -41,7 +41,7 @@ export function TxViewVin({ txInfo }) {
 
                         </Grid.Row>
 
-                        <Grid.Row className="px-6 bg-rowblack border-0 border-t border-tableblack" columns={2}>
+                        <Grid.Row className="px-6 bg-rowblack border-0 border-t border-tableblack mobile:p-2" columns={2}>
 
                             <Grid.Column className="flex items-center gap-3 p-0" width={4}>
                                 <HelpTooltip content={content.consumedTxIndex} />
@@ -50,12 +50,12 @@ export function TxViewVin({ txInfo }) {
 
                             <Grid.Column className="p-0">
                                 <div className="flex items-start gap-3">
-                                    <p>{`0x${tx['TXInLinker']['TXInPreImage']['ConsumedTxIdx'] ?? 0}`}</p>
+                                    <p className="break-all">{`0x${tx['TXInLinker']['TXInPreImage']['ConsumedTxIdx'] ?? 0}`}</p>
                                     <Popup
                                         trigger={
                                             <Icon
                                                 name="copy outline"
-                                                className="cursor-pointer hover:opacity-80"
+                                                className="cursor-pointer hover:opacity-80 mobile:hidden"
                                                 onClick={() => copyText(tx['TXInLinker']['TXInPreImage']['ConsumedTxIdx'] ?? 0)}
                                             />
                                         }
@@ -67,8 +67,7 @@ export function TxViewVin({ txInfo }) {
 
                         </Grid.Row>
 
-                        <Grid.Row className="px-6 bg-rowblack border-0 border-t border-tableblack rounded-b-md"
-                                  columns={2}>
+                        <Grid.Row className="px-6 bg-rowblack border-0 border-t border-tableblack rounded-b-md mobile:p-2" columns={2}>
 
                             <Grid.Column className="flex items-center gap-3 p-0" width={4}>
                                 <HelpTooltip content={content.signature} />
@@ -82,7 +81,7 @@ export function TxViewVin({ txInfo }) {
                                         trigger={
                                             <Icon
                                                 name="copy outline"
-                                                className="cursor-pointer hover:opacity-80"
+                                                className="cursor-pointer hover:opacity-80 mobile:hidden"
                                                 onClick={() => copyText(tx['Signature'])}
                                             />
                                         }
