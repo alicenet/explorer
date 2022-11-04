7bd4057c17a4d393cb4d5f68070d20aa0d1d57c5
Adrian Bona <adrianbona@outlook.com>
2022-11-04 12:12:46 -0300

    Good by configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     important: true,
-    theme: {
-        screens: {
-            'mobile': { 'max': '899px' },
-        },
-    },
+    theme: {},
 }
