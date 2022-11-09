7181e22bd2963444118cd65d1736636a4e825d8e
Adrian Bona <adrianbona@outlook.com>
2022-11-09 11:54:24 -0300

    Yarn Remove

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./style/index.scss";
-import "./style/tailwind.css";
+import "./style/base.css";
 
 ReactDOM.render(
     <Provider store={store}>
