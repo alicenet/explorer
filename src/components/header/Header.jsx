65be4640d02a6102e9c35a020d6365c164067e1a
Adrian Bona <adrianbona@outlook.com>
2022-12-13 19:05:46 -0300

    Reverted

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -68,9 +68,7 @@ export function Header() {
             <AppBar
                 component="nav"
                 position="sticky"
-                color="dark"
                 sx={{ backgroundImage: "none", opacity: 0.9, boxShadow: "none" }}
-                enableColorOnDark
             >
 
                 <Container disableGutters sx={{ paddingY: 1 }}>
