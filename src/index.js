3e8cb28c503cbc6f7964d3764d26ef3e7b6e9bde
Adrian Bona <adrianbona@outlook.com>
2023-01-12 16:50:32 -0300

    Going back to external theme

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./style/index.scss";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { theme } from "./style/theme";
+import { theme } from "alice-mui-provider";
 
 ReactDOM.render(
     <Provider store={store}>
