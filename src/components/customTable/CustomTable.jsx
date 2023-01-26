7e2fdf1c48146698b22a56fea9778ebcf6fb0afe
Adrian Bona <adrianbona@outlook.com>
2023-01-26 15:46:38 -0300

    Two tables, more padding, adding borders

--- src/components/customTable/CustomTable.jsx
+++ src/components/customTable/CustomTable.jsx
@@ -1,4 +1,15 @@
-import { Box, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography, useTheme } from "@mui/material";
+import {
+    Box,
+    lighten,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    Typography,
+    useTheme
+} from "@mui/material";
 import React from "react";
 
 export function CustomTable({ title, icon, headerCells, rows = [], key, double }) {
@@ -6,99 +17,113 @@ export function CustomTable({ title, icon, headerCells, rows = [], key, double }
     const theme = useTheme();
     const doubleHeaderCells = headerCells.concat(double ? headerCells : []);
 
+    const tableRowBackgroundColor = firstHalf =>
+        lighten(theme.palette.background.paper, (firstHalf ? 2 : 3) * 0.025);
+
     return (
 
-        <Paper elevation={2} square sx={{ boxShadow: "unset" }}>
+        <Table key={key}>
 
-            <Table key={key}>
+            <TableHead>
 
-                <TableHead>
+                <TableRow>
 
-                    <TableRow>
+                    <TableCell
+                        sx={{ border: 0 }}
+                        colSpan={doubleHeaderCells.length}
+                        key="table-header-main"
+                        padding="none"
+                    >
 
-                        <TableCell
-                            sx={{ border: 0 }}
-                            colSpan={doubleHeaderCells.length}
-                            key="table-header-main"
-                            padding="none"
-                        >
+                        <Paper elevation={8} sx={{ boxShadow: "unset" }} square>
 
-                            <Paper elevation={8} sx={{ boxShadow: "unset" }} square>
+                            <Box
+                                display="flex"
+                                alignItems="center"
+                                gap={2}
+                                paddingX={{ xs: 2, md: 4 }}
+                                paddingY={2}
+                                border={2}
+                                borderColor={theme.palette.primary.main}
+                                borderBottom={0}
+                                borderLeft={0}
+                                borderRight={0}
+                            >
+                                {icon}
+                                <Typography fontWeight="bold">
+                                    {title}
+                                </Typography>
+                            </Box>
 
-                                <Box
-                                    display="flex"
-                                    alignItems="center"
-                                    gap={2}
-                                    padding={2}
-                                    border={2}
-                                    borderColor={theme.palette.primary.main}
-                                    borderBottom={0}
-                                    borderLeft={0}
-                                    borderRight={0}
-                                >
-                                    {icon}
-                                    <Typography fontWeight="bold">
-                                        {title}
-                                    </Typography>
-                                </Box>
+                        </Paper>
 
-                            </Paper>
+                    </TableCell>
 
-                        </TableCell>
+                </TableRow>
 
-                    </TableRow>
+                <TableRow>
 
-                    <TableRow>
-
-                        {doubleHeaderCells.map((header, headerIndex) =>
-                            <TableCell key={`table-header-${header.id}-${headerIndex}`} sx={{ border: 0 }}>
+                    {doubleHeaderCells.map((header, headerIndex) => {
+                        const secondHalf = headerIndex >= headerCells.length;
+                        return (
+                            <TableCell
+                                key={`table-header-${header.id}-${headerIndex}`}
+                                sx={{
+                                    paddingX: { xs: 2, md: 4 },
+                                    background: tableRowBackgroundColor(secondHalf)
+                                }}
+                            >
 
                                 <Typography fontWeight="bold">
                                     {header.label}
                                 </Typography>
 
                             </TableCell>
-                        )}
-
-                    </TableRow>
-
-                </TableHead>
-
-                <TableBody>
-
-                    {rows.map((row, rowIndex) => {
-                        if (rowIndex % 2 !== 0 && double) {
-                            return null;
-                        }
-                        return (
-                            <TableRow key={`table-row-${rowIndex}`}>
-
-                                {doubleHeaderCells.map((headerCell, headerCellIndex) => {
-                                        const selectedRow = headerCellIndex >= headerCells.length ? rows[rowIndex + 1] : row;
-                                        return (
-                                            <TableCell
-                                                sx={{ border: 0, fontSize: "small" }}
-                                                key={`row-${headerCell.id}-${headerCellIndex}`}
-                                            >
-
-                                                <Typography variant="span">
-                                                    {headerCell?.displayCallback ? headerCell.displayCallback({ theme, ...selectedRow }) : selectedRow[headerCell.id]}
-                                                </Typography>
-
-                                            </TableCell>
-                                        );
-                                    }
-                                )}
-
-                            </TableRow>
                         );
                     })}
 
-                </TableBody>
+                </TableRow>
 
-            </Table>
+            </TableHead>
 
-        </Paper>
+            <TableBody>
+                {rows.map((row, rowIndex) => {
+                    if (rowIndex % 2 !== 0 && double) {
+                        return null;
+                    }
+
+                    return (
+                        <TableRow key={`table-row-${rowIndex}`}>
+
+                            {doubleHeaderCells.map((headerCell, headerCellIndex) => {
+                                const secondHalf = headerCellIndex >= headerCells.length;
+                                const selectedRow = secondHalf ? rows[rowIndex + 1] : row;
+                                return (
+
+                                    <TableCell
+                                        key={`row-${headerCell.id}-${headerCellIndex}`}
+                                        sx={{
+                                            paddingX: { xs: 2, md: 4 },
+                                            paddingY: 2.5,
+                                            borderBottom: rowIndex + (double ? 2 : 1) === rows.length ? "unset" : "",
+                                            fontSize: "small",
+                                            background: tableRowBackgroundColor(secondHalf)
+                                        }}
+                                    >
+
+                                        {headerCell?.displayCallback ? headerCell.displayCallback({ theme, ...selectedRow }) : selectedRow[headerCell.id]}
+
+                                    </TableCell>
+                                );
+                            })}
+
+                        </TableRow>
+                    );
+                })}
+
+            </TableBody>
+
+        </Table>
     );
 
 }
